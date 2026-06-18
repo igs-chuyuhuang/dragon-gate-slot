2e0697f512fcd93e71755f466fa392c500415582
chuyu <chuyuhuang@igs.com.tw>
2026-06-18 14:00:32 +0800

    balance: BG 初始 chips + 保底改為 bet×5

--- dev/js/bonusGame.js
+++ dev/js/bonusGame.js
@@ -4,9 +4,9 @@ function getPassMult(gap) {
 
 export class BonusGame {
   constructor(bet, pool) {
-    this.chips = bet * 6;
+    this.chips = bet * 5;
     this.initialChips = this.chips;
-    this.guarantee = bet * 6;
+    this.guarantee = bet * 5;
     this.winnings = 0;
     this.pool = pool;
     this.roundBet = 0;
